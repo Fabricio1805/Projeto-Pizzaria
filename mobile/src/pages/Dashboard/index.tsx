6fc0968ef70f4408ec277e0e772a16ea0c43431d
Fabricio1805 <fabricio.ss09@outloo.com>
2023-01-06 17:43:30 -0300

    dashboard

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,15 +1,79 @@
-import React,{useContext} from 'react';
-import { View, Text, Button } from 'react-native';
-import { AuthContext } from '../../contexts/AuthContext';
+import React,{useState} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity
+} from 'react-native';
 
 const Dashboard = () => {
-  const { signOut } = useContext(AuthContext);
+
   return (
-    <View>
-      <Text>Tela Dashboard</Text>
-      <Button title='Sair' onPress={signOut}/>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Novo Pedido</Text>
+
+      <TextInput
+        placeholder='Numero de mesa'
+        placeholderTextColor="#1D1D2E"
+        style={styles.input}
+        keyboardType='numeric'
+      />
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Abrir mesa</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 15,
+    backgroundColor: '#1D1D2E'
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#FFF',
+    marginBottom: 24
+  },
+  input: {
+    width: '90%',
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 4,
+    paddingHorizontal: 8,
+    textAlign: 'center',
+    fontSize: 22,
+    color: '#1D1D2E'
+  },
+  button: {
+    width: '90%',
+    height: 40,
+    backgroundColor: '#3FFFA3',
+    borderRadius: 4,
+    marginVertical: 12,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#101026',
+    fontWeight: 'bold'
+  },
+})
+
+
+
+
+
+
+
+
+
 
 export default Dashboard
