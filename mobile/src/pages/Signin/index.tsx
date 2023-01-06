fe3c961681044ae9a45e6e7e1496bbedca1acb43
Fabricio1805 <fabricio.ss09@outloo.com>
2023-01-06 16:09:36 -0300

    login

--- mobile/src/pages/Signin/index.tsx
+++ mobile/src/pages/Signin/index.tsx
@@ -1,14 +1,114 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useContext, useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity
+} from 'react-native';
+import { AuthContext } from '../../contexts/AuthContext';
 
 type Props = {}
 
 const SignIn = (props: Props) => {
+
+  const { signIn } = useContext(AuthContext);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleLogin() {
+    if (email === '' || password === '') {
+      return;     
+    }
+
+    await signIn({ email, password });
+  }
   return (
-    <View>
-        <Text>SignIn</Text>      
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require('../../assets/logo.png')}
+      />
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Digite seu email"
+          style={styles.input}
+          placeholderTextColor='#101026'
+          value={email}
+          onChangeText={setEmail}
+        />
+        <TextInput
+          placeholder="Digite sua senha"
+          style={styles.input}
+          placeholderTextColor='#101026'
+          secureTextEntry={true}
+          value={password}
+          onChangeText={setPassword}
+        />
+      </View>
+      
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Acessar</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#1D1D2E'
+  },
+  logo: {
+    marginBottom: 18
+  },
+  inputContainer: {
+    width: '95%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 32,
+    paddingHorizontal: 14
+  },
+  input: {
+    width: '95%',
+    height: 40,
+    backgroundColor: '#FFF',
+    marginBottom: 12,
+    borderRadius: 4,
+    paddingHorizontal: 8,
+    color: '#101026'
+  },
+  button: {
+    width: '85%',
+    height: 40,
+    backgroundColor: '#3fffa3',
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#101026'
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
 export default SignIn;
+
--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useContext} from 'react';
+import { View, ActivityIndicator } from 'react-native';
 import AppRoutes from './app.routes';
 import AuthRoutes from './auth.routes';
+import { AuthContext } from '../contexts/AuthContext';
 
 const Routes = () => {
-    const isAuthenticated = false;
+  const { isAuthenticated } = useContext(AuthContext);
+  const loading = false;
+
+  if (loading) {
+    return (
+      <View style={{
+        flex: 1,
+        backgroundColor: '#1D1D2E',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+        <ActivityIndicator size={60} color="#FFF"/>
+      </View>
+    )
+  }
   return (
     isAuthenticated ? <AppRoutes/> : <AuthRoutes/>
   )
